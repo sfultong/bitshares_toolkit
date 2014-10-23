33915cf37ec3567856838d95a8dc700ec8a6fbca
Sam Griffin <sam.griffin@gmail.com>
2014-10-23 10:22:13 -0400

    adding pretty-print and load/save options

--- programs/utils/btc_create_snapshot.cpp
+++ programs/utils/btc_create_snapshot.cpp
@@ -12,6 +12,7 @@
 #include <array>
 #include <vector>
 #include <iterator>
+#include <fc/crypto/base58.hpp>
 
 using namespace std;
 
@@ -54,10 +55,7 @@ struct snapshot_header {
         this->merkle_root[1] = 'b';
         this->merkle_root[19] = 'z';
     }
-    
-
 };
-
 std::ostream& operator<<(std::ostream &os, const snapshot_header &header) {
     os.write(reinterpret_cast<const char*>(&header.version), sizeof(uint32_t));
     std::copy(header.merkle_root.begin(), header.merkle_root.end(), std::ostream_iterator<char>(os));
@@ -89,8 +87,17 @@ std::istream& operator>>(std::istream &is, snapshot_header &header) {
     is.read(reinterpret_cast<char*>(&header.raw_script_offset), sizeof(uint64_t));
     return is;
 }
-// std::ostream& operator<<(std::ostream &os, const myStruct &myThing)
-// std::istream& operator>>(std::istream &is, myStruct &myThing)
+void prettyPrint (const snapshot_header &header) {
+    std::cout << "version\t" << header.version << "\n"
+            << "merkle root\ttodo\n"
+            << "block hash\ttodo\n"
+            << "total claim value\t" << header.total_claim_value << "\n"
+            << "number of claims\t" << header.num_claims << "\n"
+            << "p2pkh offset\t" << header.p2pkh_offset << "\n"
+            << "p2sh offset\t" << header.p2sh_offset << "\n"
+            << "multisig offset\t" << header.multisig_offset << "\n"
+            << "raw script offset\t" << header.raw_script_offset << "\n";
+}
 
 struct p2pkh {
     std::array<char,20> hash;
@@ -108,7 +115,6 @@ struct p2pkh {
         amount(other.amount)
     { }
 };
-
 std::ostream& operator<<(std::ostream &os, const p2pkh &entry) {
     std::copy(entry.hash.begin(), entry.hash.end(), std::ostream_iterator<char>(os));
     os.write(reinterpret_cast<const char*>(&entry.amount), sizeof(uint64_t));
@@ -121,20 +127,27 @@ std::istream& operator>>(std::istream &is, p2pkh &entry) {
     is.read(reinterpret_cast<char*>(&entry.amount), sizeof(uint64_t));
     return is;        
 }
+void prettyPrint (const p2pkh &entry) {
+    std::vector<char> vec;
+    for (auto &c : entry.hash) {
+        vec.push_back(c);
+    }
+    std::cout << fc::to_base58(vec) << "\t"
+            << entry.amount << "\n";
+}
 
 struct snapshot {
     snapshot_header         header;
     std::vector<p2pkh>      p2pkh_entries;
     
     void add_p2pkh (p2pkh entry) {
-        header.p2sh_offset += sizeof(p2pkh);
-        header.multisig_offset += sizeof(p2pkh);
-        header.raw_script_offset += sizeof(p2pkh);
+        header.p2sh_offset += 28;
+        header.multisig_offset += 28;
+        header.raw_script_offset += 28;
+        header.num_claims++;
         header.total_claim_value += entry.amount;
         p2pkh_entries.push_back(entry);
     }
-    
-
 };
 std::ostream& operator<<(std::ostream &os, const snapshot &snap) {
     os << snap.header;
@@ -145,33 +158,54 @@ std::ostream& operator<<(std::ostream &os, const snapshot &snap) {
 }
 std::istream& operator>>(std::istream &is, snapshot &snap) {
     is >> snap.header;
+    p2pkh entry;
     for (uint64_t i = HEADER_OFFSET; i < snap.header.p2sh_offset; i += 28) {
-        p2pkh entry;
         is >> entry;
         snap.p2pkh_entries.push_back(p2pkh(entry));
     }
     return is;
 }
+void prettyPrint (const snapshot &snap) {
+    prettyPrint(snap.header);
+    std::cout << "-----p2pkh entries-----\n";
+    for (auto &entry : snap.p2pkh_entries) {
+        prettyPrint(entry);
+    }
+}
 
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-    std::cout << "Test\n";
+void writeTestSnapshot () {
     std::ofstream snapshot_bin("snapshot.bin", std::ofstream::binary);
     struct snapshot snapshot;
-    
-    
+        
     std::array<char,20> hash;
     for (int i = 0; i < 20; i++) {
         hash[i] = 'a' + i;
     }
     snapshot.add_p2pkh(p2pkh(hash, 50));
-    
+    prettyPrint(snapshot);
     
     snapshot_bin << snapshot;
-    //snapshot_bin.write((char*)&snapshot, sizeof(snapshot));
+}
+
+void loadAndPrint () {
+    std::ifstream snapshot_bin("snapshot.bin", std::ifstream::binary);
+    struct snapshot snapshot;
+    
+    snapshot_bin >> snapshot;
+    prettyPrint(snapshot);
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    if (2 == argc) {
+        std::cout << "Writing snapshot\n";
+        writeTestSnapshot();
+    } else {
+        std::cout << "Loading snapshot\n";
+        loadAndPrint();
+    }
     
     return 0;
 }
